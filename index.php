a74487d77fcc3e9899438dc013ce51e1db7843d4
samantha-r-dzirko <54124915+samantha-r-dzirko@users.noreply.github.com>
2020-09-02 09:26:54 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    include('partials/header.php');
+?>
+
+    <main>
+        <h2>This H2 is on the homepage</h2>
+        <p>Paragraph one</p>
+        <p>Paragraph two</p>
+        <p>Paragraph three</p>
+    </main>
+
+
+    
+</body>
+</html>
---
+++ partials/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv=”X-UA-Compatible” content=”ie=edge”>
+    <title>Chicago BIMPOC Backstage Theatre Artists</title>
+<link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    <header>
+        <h1>PHP SITE WITH SHARED HEADER</h1>
+        <nav>
+            <ul>
+                <li><a href=”index.php”>Home</a></li>
+                <li><a href=”contact.php”>Contact</a></li>
+            </ul>
+        </nav>
+    </header>
